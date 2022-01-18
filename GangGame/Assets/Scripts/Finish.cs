66e17fb352943ed315998c2409804558608f18b5
PaulaKl <Paula.Klugmann@gmx.de>
2022-01-18 18:58:55 +0100

    PauseScreen Designs in all Levels

---
+++ GangGame/Assets/Scripts/Finish.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Finish : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag.Equals("Player"))
+        {
+            coll.gameObject.GetComponent<PlayerUI>().showWinScreen();
+            GameObject.FindGameObjectsWithTag("Enemy").ToList().ForEach(x => x.SetActive(false));
+            Time.timeScale = 0f;
+        }
+    }
+}
