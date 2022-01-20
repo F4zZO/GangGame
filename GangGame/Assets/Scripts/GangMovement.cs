aa1753ff081b417be84fcc4ad320f76fe4705f34
Fazzo <frederic.woerz@gmx.net>
2022-01-20 04:53:54 +0100

    Add Character with basic movement, physics and animation

--- GangGame/Assets/Scripts/GangMovement.cs
+++ GangGame/Assets/Scripts/GangMovement.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 public class GangMovement : MonoBehaviour
@@ -7,26 +6,52 @@ public class GangMovement : MonoBehaviour
     [Header("--- INIT ---")]
     [SerializeField] private Rigidbody body;
     [SerializeField] private Transform cam;
-    [SerializeField] private Animator anim;
+    [SerializeField] private Animator animator;
+    [SerializeField] private BoxCollider spring;
 
     [Header("--- GENERAL ---")]
-    [SerializeField] private float walkSpeed;
-    [SerializeField] private float runSpeed;
-    [SerializeField] private float jumpForce;
-
-    [SerializeField] private float fallForce;
-
     [SerializeField] private float currentSpeed;
     [SerializeField] private PlayerState playerState;
 
-    [SerializeField] private float turnSmoothTime = 0.1f;
-
     [SerializeField] private bool isGrounded = false;
     [SerializeField] private bool canMove = false;
+    [SerializeField] private bool canJump = false;
+    [SerializeField] private bool isFalling = false;
+    [SerializeField] private bool isLanding = false;
+    [SerializeField] private bool isTwerking = false;
+
+    [SerializeField] private bool doJump = false;
+    [SerializeField] private bool fastJump = false;
+
+    [Header("--- STATS ---")]
+
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    [SerializeField] private float airSpeed;
+    [SerializeField] private float airFastSpeed;
+    [SerializeField] private float fallSpeed;
+    [SerializeField] private float landSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private float fallForce;
+
+    [SerializeField] private float groundDistance;
+
+    [SerializeField] private float jumpCD;
+
+    [SerializeField] private float fallTime;
+    [SerializeField] private float landTime;
+
+    [SerializeField] private float turnSmoothTime = 0.1f;
 
     //---------------------------------------------------------
     private float turnSmoothVelocity;
     private Vector3 moveDir;
+    private Vector3 direction;
+    private PlayerState lastState;
+
+    private Timer timerJumpCD; 
+    private Timer timerFall;
+    private Timer timerLand;
 
     private enum PlayerState
     {
@@ -34,144 +59,264 @@ public class GangMovement : MonoBehaviour
         walk,
         run,
         jump,
-        fastJump,
         fall,
+        land,
         standUp,
+        twerk,
         ragdoll
     }
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+
+        this.timerJumpCD = new Timer();
+        this.timerJumpCD.Elapsed += this.EndJumpCD;
+
+        this.timerFall = new Timer();
+        this.timerFall.Elapsed += this.EndFall;
+
+        this.timerLand = new Timer();
+        this.timerLand.Elapsed += this.EndLand;
+
+        this.canJump = true;
     }
     
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+
         this.moveDir = Vector3.zero;
+        this.direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-        //Vector3 jumpV = new Vector3(0f, this.jumpForce, 0f).normalized;
-
-        if (this.body.velocity.y < 0)
-        {
-            this.body.velocity += Vector3.up * Physics.gravity.y * fallForce * Time.deltaTime;
-        }
-        else if(this.body.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
-        {
-            this.body.velocity += Vector3.up * Physics.gravity.y * fallForce * Time.deltaTime;
-        }
-
-        if (this.playerState != PlayerState.fall && this.playerState != PlayerState.ragdoll && this.isGrounded)
-        {
-            this.canMove = true;
-        }
-        else
-        {
-            this.canMove = false;
-        }
-
-        if (direction.magnitude < 0.1f && this.canMove)
+        if (this.direction.magnitude < 0.1f && this.canMove)
         {
             this.playerState = PlayerState.idle;
+            this.currentSpeed = 0;
         }
 
-        if (direction.magnitude >= 0.1f && this.canMove)
+        if (this.direction.magnitude >= 0.1f && this.canMove)
         {
-            this.playerState = PlayerState.walk;
-            this.currentSpeed = this.walkSpeed;
-
             if (Input.GetKey(KeyCode.LeftShift))
             {
                 this.playerState = PlayerState.run;
                 this.currentSpeed = this.runSpeed;
             }
+            else
+            {
+                this.playerState = PlayerState.walk;
+                this.currentSpeed = this.walkSpeed;
+            }
+        }
 
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + this.cam.eulerAngles.y;
+        if (Input.GetKey(KeyCode.Space) && this.canMove && this.canJump && !this.isLanding)
+        {
+            this.doJump = true;
+            this.playerState = PlayerState.jump;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                this.fastJump = true;
+            }
+            else
+            {
+                this.fastJump = false;
+            }
+        }
+
+        if(this.body.velocity.y > 0.1)
+        {
+            this.playerState = PlayerState.jump;
+        }
+
+        if (this.body.velocity.y < -0.5 && !this.isGrounded)
+        {
+            if (!this.timerFall.Enabled)
+            {
+                this.timerFall.Interval = this.fallTime;
+                this.timerFall.Start();
+            }
+            if (this.isFalling)
+            {
+                this.playerState = PlayerState.fall;
+            }
+        }
+        else
+        {
+            this.timerFall.Stop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            isTwerking = !isTwerking;
+        }
+
+        if (this.isTwerking && this.direction.magnitude < 0.1f && this.body.velocity.y < 1)
+        {
+            this.playerState = PlayerState.twerk;
+        }
+        else if (this.isTwerking)
+        {
+            this.isTwerking = false;
+        }
+
+        if (this.isLanding)
+        {
+            this.playerState = PlayerState.land;
+        }
+
+        if (this.playerState != this.lastState)
+        {
+            switch (this.playerState)
+            {
+                case PlayerState.idle:
+                    this.animator.Play("idle");
+                    break;
+                case PlayerState.walk:
+                    this.animator.Play("walk");
+                    break;
+                case PlayerState.run:
+                    this.animator.Play("run");
+                    break;
+                case PlayerState.jump:
+                    this.animator.Play("jump");
+                    break;
+                case PlayerState.fall:
+                    this.animator.Play("fall");
+                    break;
+                case PlayerState.land:
+                    this.animator.Play("land");
+                    break;
+                case PlayerState.twerk:
+                    this.animator.Play("twerk");
+                    break;
+            }
+        }
+
+        this.lastState = this.playerState;
+    }
+
+    void FixedUpdate()
+    {
+        if (this.body.velocity.y < 0)
+        {
+            this.body.velocity += Vector3.up * Physics.gravity.y * fallForce * Time.deltaTime;
+        }
+        else if (this.body.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            this.body.velocity += Vector3.up * Physics.gravity.y * fallForce * Time.deltaTime;
+        }
+
+        if (Physics.Raycast(transform.position, Vector3.down, this.groundDistance) && this.body.velocity.y < 0.1)// && this.body.velocity.y > -0.5)
+        {
+            this.isGrounded = true;
+
+            if (this.isGrounded)
+            {
+                this.canMove = true;
+            }
+            
+            if (this.isFalling)
+            {
+                this.isFalling = false;
+                this.isLanding = true;
+                this.timerLand.Interval = this.landTime;
+                this.timerLand.Start();
+            }
+
+            if (!this.canJump && !this.timerJumpCD.Enabled)
+            {
+                this.timerJumpCD.Interval = this.jumpCD;
+                this.timerJumpCD.Start();
+            }
+        }
+        else
+        {
+            this.isGrounded = false;
+            this.canMove = false;
+            this.currentSpeed = this.airSpeed;
+
+            if (this.fastJump)
+            {
+                this.currentSpeed = this.airFastSpeed;
+            }
+
+            if (this.isFalling)
+            {
+                this.currentSpeed = this.fallSpeed;
+            }
+        }
+
+        if (this.isLanding)
+        {
+            this.currentSpeed = this.landSpeed;
+        }
+
+        if (this.direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(this.direction.x, this.direction.z) * Mathf.Rad2Deg + this.cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(this.transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, this.turnSmoothTime);
             this.transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             this.moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             this.moveDir.Normalize();
 
-
             this.body.MovePosition(this.transform.position + this.moveDir * Time.deltaTime * this.currentSpeed);
-
-            //this.body.AddForce(this.moveDir * Time.deltaTime * this.currentSpeed);
-            //this.body.velocity = this.moveDir * Time.deltaTime * this.currentSpeed * 100;
         }
 
-        if (Input.GetKey(KeyCode.Space) && this.canMove)
+        if (this.doJump)
         {
-            this.playerState = PlayerState.jump;
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                this.playerState = PlayerState.fastJump;
-            }
-
             this.body.velocity = Vector3.up * this.jumpForce;
-            //this.body.AddForce
-        }
 
-
-
-        switch (this.playerState)
-        {
-            case PlayerState.idle:
-                this.currentSpeed = 0;
-                this.anim.SetTrigger("Idle");
-                break;
-            case PlayerState.walk:
-                this.anim.SetTrigger("Walk");
-                break;
-            case PlayerState.run:
-                this.anim.SetTrigger("Run");
-                break;
-            case PlayerState.jump:
-                //this.anim.SetTrigger("Jump");
-                this.anim.SetTrigger("Idle");
-                if (this.canMove)
-                {
-                    this.playerState = PlayerState.idle;
-                }
-                break;
-            case PlayerState.fastJump:
-                //this.anim.SetTrigger("FastJump");
-                this.anim.SetTrigger("Idle");
-                break;
+            this.canJump = false;
+            this.doJump = false;
         }
     }
 
+    public void EndJumpCD(object source, ElapsedEventArgs e)
+    {
+        this.canJump = true;
+        this.timerJumpCD.Stop();
+    }
+
+    public void EndFall(object source, ElapsedEventArgs e)
+    {
+        this.isFalling = true;
+        this.timerFall.Stop();
+    }
+
+    public void EndLand(object source, ElapsedEventArgs e)
+    {
+        this.isLanding = false;
+        this.timerLand.Stop();
+    }
+
+    /*
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.tag == "a")
-        {
-        }
+        if (collision.tag == "Floor")
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Floor")
-        {
-            //this.isGrounded = true;
-        }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (collision.collider.tag == "Floor")
-        {
-            this.isGrounded = false;
-        }
     }
 
     private void OnCollisionStay(Collision collision)
     {
-        if (collision.collider.tag == "Floor")
-        {
-            this.isGrounded = true;
-        }
     }
+
+    if (this.playerState != PlayerState.fall && this.playerState != PlayerState.ragdoll && this.isGrounded)
+    {
+        this.canMove = true;
+    }
+    else
+    {
+        this.canMove = false;
+    }       
+     */
 }
