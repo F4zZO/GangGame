709de7d18983238a0ea0b15e2623fd7b29e2e6a7
BenKJI <bak00@web.de>
2022-01-26 17:16:46 +0100

    PressurePlate

---
+++ GangGame/Assets/Scripts/PPMoveGameObject.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PPMoveGameObject : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = .5f;
+    [SerializeField] private Vector3 _startPos;
+
+    private Vector3 _target;
+    private bool _started = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _target = this.transform.position;
+        this.transform.localPosition = _target - _startPos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_started)
+            this.transform.position = Vector3.MoveTowards(this.transform.position, _target, _moveSpeed);
+    }
+
+    public void PressurePlateActivate()
+    {
+        _started = true;
+    }
+}
