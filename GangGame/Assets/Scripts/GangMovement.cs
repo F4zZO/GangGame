dd3b2074faeab07b6c6dd1bca7394804725ba2e7
BenKJI <bak00@web.de>
2022-01-27 15:44:58 +0100

    RopeSwing implemented

--- GangGame/Assets/Scripts/GangMovement.cs
+++ GangGame/Assets/Scripts/GangMovement.cs
@@ -19,6 +19,8 @@ public class GangMovement : MonoBehaviour
     [SerializeField] private bool isFalling = false;
     [SerializeField] private bool isLanding = false;
     [SerializeField] private bool isTwerking = false;
+    [SerializeField] private bool isHanging = false;
+    [SerializeField] private Hangable hangingTo = null;
 
     [SerializeField] private bool doJump = false;
     [SerializeField] private bool fastJump = false;
@@ -49,7 +51,7 @@ public class GangMovement : MonoBehaviour
     private Vector3 direction;
     private PlayerState lastState;
 
-    private Timer timerJumpCD; 
+    private Timer timerJumpCD;
     private Timer timerFall;
     private Timer timerLand;
 
@@ -63,7 +65,8 @@ public class GangMovement : MonoBehaviour
         land,
         standUp,
         twerk,
-        ragdoll
+        ragdoll,
+        hang
     }
 
     void Start()
@@ -91,7 +94,7 @@ public class GangMovement : MonoBehaviour
 
         this.canJump = true;
     }
-    
+
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -106,7 +109,7 @@ public class GangMovement : MonoBehaviour
             this.currentSpeed = 0;
         }
 
-        if (this.direction.magnitude >= 0.1f && this.canMove)
+        if (this.direction.magnitude >= 0.1f && this.canMove && !this.isHanging)
         {
             if (Input.GetKey(KeyCode.LeftShift))
             {
@@ -134,9 +137,22 @@ public class GangMovement : MonoBehaviour
             }
         }
 
-        if(this.body.velocity.y > 0.1 && Input.GetKey(KeyCode.Space))
+        if (this.body.velocity.y > 0.1 && Input.GetKey(KeyCode.Space))
         {
             this.playerState = PlayerState.jump;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            checkForHangable();
+        }
+        else
+        {
+            this.GetComponent<Rigidbody>().isKinematic = false;     //Safety Net, if attaching to Rope doesn't work;
+        }
+        if (Input.GetKeyUp(KeyCode.Space) && this.isHanging)
+        {
+            stopHanging();
         }
 
         if (this.body.velocity.y < -0.5 && !this.isGrounded)
@@ -155,7 +171,10 @@ public class GangMovement : MonoBehaviour
         {
             this.timerFall.Stop();
         }
-
+        if (this.isHanging)
+        {
+            this.playerState = PlayerState.hang;
+        }
         if (Input.GetKeyDown(KeyCode.T))
         {
             isTwerking = !isTwerking;
@@ -200,6 +219,10 @@ public class GangMovement : MonoBehaviour
                 case PlayerState.twerk:
                     this.animator.Play("twerk");
                     break;
+                case PlayerState.hang:
+                    this.animator.Play("hangingIdle");
+                    break;
+
             }
         }
 
@@ -225,7 +248,7 @@ public class GangMovement : MonoBehaviour
             {
                 this.canMove = true;
             }
-            
+
             if (this.isFalling)
             {
                 this.isFalling = false;
@@ -262,7 +285,7 @@ public class GangMovement : MonoBehaviour
             this.currentSpeed = this.landSpeed;
         }
 
-        if (this.direction.magnitude >= 0.1f)
+        if (this.direction.magnitude >= 0.1f && !this.isHanging)
         {
             float targetAngle = Mathf.Atan2(this.direction.x, this.direction.z) * Mathf.Rad2Deg + this.cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(this.transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, this.turnSmoothTime);
@@ -310,7 +333,7 @@ public class GangMovement : MonoBehaviour
                 GameManager.Instance.ReloadScene();
             }
             catch
-            {}
+            { }
         }
 
         if (collision.collider.tag == "Win")
@@ -352,4 +375,53 @@ public class GangMovement : MonoBehaviour
         this.canMove = false;
     }       
      */
+    private void checkForHangable()
+    {
+        RaycastHit[] hits;
+        hits = Physics.SphereCastAll(this.transform.position, 1, Vector3.forward, 5);
+        foreach (RaycastHit hit in hits)
+        {
+            Hangable tryHang = hit.transform.gameObject.GetComponent<Hangable>();
+            if (tryHang != null)
+            {
+                hangTo(tryHang);
+                return;
+            }
+        }
+        if (this.hangingTo != null)
+        {
+            hangTo(this.hangingTo);
+        }
+    }
+
+    private void hangTo(Hangable tryHang)
+    {
+        this.hangingTo = tryHang;
+        Rigidbody objRigidBody = this.hangingTo.GetComponent<Rigidbody>();
+        this.GetComponent<Rigidbody>().isKinematic = true;
+        if (Vector3.Distance(this.transform.position, this.hangingTo.transform.position + Vector3.up * -2f) > 0.1f && !isHanging)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, this.hangingTo.transform.position + Vector3.up * -2f, 0.8f);
+        }
+        else
+        {
+            if (this.gameObject.GetComponent<FixedJoint>() == null)
+                this.gameObject.AddComponent<FixedJoint>().connectedBody = objRigidBody;
+
+            this.hangingTo.Attach(direction,this.GetComponent<Rigidbody>());
+            this.isHanging = true;
+            this.playerState = PlayerState.hang;
+            this.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    } 
+
+    private void stopHanging()
+    {
+        this.isHanging = false;
+        this.playerState = PlayerState.fall;
+        this.hangingTo.Detach();
+        this.hangingTo = null;
+        Destroy(this.GetComponent<FixedJoint>());
+    }
+
 }
---
+++ GangGame/Assets/Scripts/Hangable.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hangable : MonoBehaviour
+{
+    [SerializeField] private bool attached = false;
+    [SerializeField] private float speedLimit = 20;
+    [SerializeField] private float swingAcceleration = 15;
+    [SerializeField] private HingeJoint hJoint;
+
+    private Rigidbody rb;
+    private Rigidbody hangingRB;
+    private float hJointMax = 0;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if (attached && Input.GetKey(KeyCode.W))
+        {
+            Debug.Log(hJoint.angle);
+            rb.drag = -0.5f;
+            float accelFactor = Mathf.Clamp(((hJointMax / 90) * swingAcceleration), 5, swingAcceleration);
+            Vector3 forceAdd = rb.velocity * accelFactor;
+            rb.AddForce(forceAdd, ForceMode.Acceleration);
+        }
+        else
+        {
+            rb.drag = 0.2f;
+ 
+        }
+        if (attached)
+        {
+            if(Mathf.Abs(hJoint.angle) > hJointMax)
+            {
+                hJointMax = Mathf.Abs(hJoint.angle);
+                hJointMax = Mathf.Clamp(hJointMax, 0,90);
+            }
+        }
+
+        if(rb.velocity.magnitude > speedLimit)
+        {
+            rb.velocity = rb.velocity.normalized * speedLimit;
+        }
+    }
+    public void Attach(Vector3 dir,Rigidbody hangingRB)
+    {
+        this.hangingRB = hangingRB;
+        if (!attached)
+        {
+            GetComponent<Rigidbody>().AddForce(dir * 750, ForceMode.Acceleration);
+            attached = true;
+        }
+    }
+    public void Detach()
+    {
+        attached = false;
+        hangingRB.AddForce(Vector3.up * 25);
+        this.hangingRB = null;
+    }
+}
