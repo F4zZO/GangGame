fa134e661f73b4faaa06b2f3d5d32218e971c68f
PaulaKl <Paula.Klugmann@gmx.de>
2022-02-28 16:52:07 +0100

    sounds

--- GangGame/Assets/Scripts/Death.cs
+++ GangGame/Assets/Scripts/Death.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Death : MonoBehaviour
 {
+
+    [SerializeField] private GameObject Music;
+
     private void OnTriggerEnter(Collider coll)
     {
         if (coll.gameObject.tag.Equals("Player"))
         {
+            this.gameObject.GetComponent<AudioSource>().Play();
+            this.Music.GetComponent<AudioSource>().Stop();
             GameManager.Instance.Lose();
             Time.timeScale = 0f;
         }
--- GangGame/Assets/Scripts/Finish.cs
+++ GangGame/Assets/Scripts/Finish.cs
@@ -7,12 +7,15 @@ public class Finish : MonoBehaviour
 {
 
     [SerializeField] private GameObject Firework;
+    [SerializeField] private GameObject Music;
 
     private void OnTriggerEnter(Collider coll)
     {
         if (coll.gameObject.tag.Equals("Player"))
         {
             this.Firework.SetActive(true);
+            this.gameObject.GetComponent<AudioSource>().Play();
+            this.Music.GetComponent<AudioSource>().Stop();
             GameManager.Instance.Win();
         }
     }
